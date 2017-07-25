539afa3ee9da53a6e4c02d9b03824738725b3ae6
Jade Anderson <enighter@gmail.com>
2017-07-25 01:48:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function forEach(iterable, callback) {
   if (Array.isArray(iterable)) {
-    for (let i = 0, l = iterable.length; i < 1; i++) {
+    for (let i = 0, l = iterable.length; i < l; i++) {
       const element = iterable[i];
       callback(element, i, iterable);
     }
