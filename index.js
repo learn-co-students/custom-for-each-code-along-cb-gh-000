478a3f999c8af4f04ad5cd7abecd6e1409122f27
Jhondee Diaz <jhondeediaz@gmail.com>
2017-08-25 01:05:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('hi');
+function forEach(iterable, callback) {
+  if (Array.isArray(iterable)) {
+    for (let i = 0, l = iterable.length; i < l; i++) {
+      const element = iterable[i];
+      callback(element, i, iterable);
+    }
+  } else if (typeof iterable === 'object') {
+    for (const key in iterable) {
+      const value = iterable[key];
+      callback(value, key, iterable);
+    }
+  }
+}
