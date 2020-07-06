01bc534ba05cca9fcd626092fa6a18d6d039f004
Sarmad Riaz <ms30237@gmail.com>
2020-07-06 07:26:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function forEach(iterable, callback) {
+  if (Array.isArray(iterable)) {
+    for (let i = 0, l = iterable.length; i < l; i++) {
+      callback(iterable[i], i, iterable)
+    }
+  } else if (typeof iterable === 'object') {
+    for (let key in iterable) {
+      callback(iterable[key], key, iterable)
+    }
+  }
+}
+© 2020 GitHub, Inc.
